53aa8bb4d65fcc58048346d7d57791dbe40e4e7d
Hower16 <miky1216@gmail.com>
2021-04-28 16:28:29 -0500

    Improved string formatting

--- PythonRPS/main.py
+++ PythonRPS/main.py
@@ -30,13 +30,13 @@ def choose_game_type():
 
 def set_player_name():
     name = input("Please enter a name")
-    print('Hello, {name}!'.format(name=name))
+    print(f'Hello, {name}!')
     return name
 
 
 def choose_gesture(player_name):
     gesture_choice = input("Please choose a gesture. Type R for rock, P for paper, or S for scissors")
-    print('{player_name} chose {gesture_choice}'.format(player_name=player_name, gesture_choice=gesture_choice))
+    print(f'{player_name} chose {gesture_choice}')
     return gesture_choice
 
 
@@ -44,7 +44,7 @@ def get_random_gesture(player_name):
     gestures = ['R', 'P', 'S']
     random_number = random.randint(0, 2)
     random_gesture = gestures[random_number]
-    print('{player_name} chose {random_gesture}'.format(player_name=player_name, random_gesture=random_gesture))
+    print(f'{player_name} chose {random_gesture}')
     return random_gesture
 
 
@@ -54,31 +54,31 @@ def determine_round_winner(player_one_name, player_one_gesture, player_two_name,
     if player_one_gesture == player_two_gesture:
         print('Tie!')
     elif player_one_gesture == 'R' and player_two_gesture == 'S':
-        print('{player_one_name} wins the round!'.format(player_one_name=player_one_name))
+        print(f'{player_one_name} wins the round!')
         player_one_score += 1
     elif player_one_gesture == 'P' and player_two_gesture == 'R':
-        print('{player_one_name} wins the round!'.format(player_one_name=player_one_name))
+        print(f'{player_one_name} wins the round!')
         player_one_score += 1
     elif player_one_gesture == 'S' and player_two_gesture == 'P':
-        print('{player_one_name} wins the round!'.format(player_one_name=player_one_name))
+        print(f'{player_one_name} wins the round!')
         player_one_score += 1
     else:
-        print('{player_two_name} wins the round!'.format(player_two_name=player_two_name))
+        print(f'{player_two_name} wins the round!')
         player_two_score += 1
 
 
 def display_scoreboard(player_one_name, player_two_name):
-    print('{player_one_name} score: {player_one_score}'.format(player_one_name=player_one_name, player_one_score=player_one_score))
-    print('{player_two_name} score: {player_two_score}'.format(player_two_name=player_two_name, player_two_score=player_two_score))
+    print(f'{player_one_name} score: {player_one_score}')
+    print(f'{player_two_name} score: {player_two_score}')
 
 
 def check_if_game_winner(player_one_name, player_two_name):
     global player_one_score
     global player_two_score
     if player_one_score == 2:
-        print('{player_one_name} wins the game!!'.format(player_one_name=player_one_name))
+        print(f'{player_one_name} wins the game!!')
     elif player_two_score == 2:
-        print('{player_two_name} wins the game!!'.format(player_two_name=player_two_name))
+        print(f'{player_two_name} wins the game!!')
 
 
 run_game()
